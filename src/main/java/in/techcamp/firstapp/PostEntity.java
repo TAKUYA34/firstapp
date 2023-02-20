3bc88374b4f565cc5c0b107acea737b6fc2a37ec
TAKUYA34 <110417799+TAKUYA34@users.noreply.github.com>
2023-02-20 20:43:50 +0900

    firstapp実装の練習

---
+++ src/main/java/in/techcamp/firstapp/PostEntity.java
@@ -0,0 +1,27 @@
+package in.techcamp.firstapp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class PostEntity {
+    private long id;
+    private String memo;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
+}
